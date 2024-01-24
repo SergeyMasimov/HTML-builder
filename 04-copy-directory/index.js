e7374169dea4c11fc7b728682644f2bb3365b0b0
Sergey Masimov <amine.kun115@gmail.com>
2024-01-24 13:51:53 +0500

    feat: make some changes in 04-copy-directory program

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,15 +8,23 @@ async function copyFiles() {
   try {
     await fs.mkdir(destDirPath, { recursive: true });
 
-    const dirents = await fs.readdir(srcDirPath, { withFileTypes: true });
+    const srcDirents = await fs.readdir(srcDirPath, { withFileTypes: true });
+    const destDirents = await fs.readdir(destDirPath, { withFileTypes: true });
 
-    for (const dirent of dirents) {
-      if (dirent.isFile()) {
-        const srcFilePath = path.resolve(srcDirPath, dirent.name);
-        const destFilePath = path.resolve(destDirPath, dirent.name);
+    const srcFileNames = srcDirents.filter(dirent => dirent.isFile()).map(dirent => dirent.name);
+    const destFileNames = destDirents.filter(dirent => dirent.isFile()).map(dirent => dirent.name);
 
-        await fs.copyFile(srcFilePath, destFilePath);
+    for (const fileName of destFileNames) {
+      if (!srcFileNames.includes(fileName)) {
+        await fs.unlink(path.resolve(destDirPath, fileName));
       }
+    }
+
+    for (const fileName of srcFileNames) {
+      const srcFilePath = path.resolve(srcDirPath, fileName);
+      const destFilePath = path.resolve(destDirPath, fileName);
+
+      await fs.copyFile(srcFilePath, destFilePath);
     }
   } catch (error) {
     console.error(error);
